d99c632269b0c96ce8d22deb63c8b798146dcaeb
ahmedmo2399 <ahmedabdelmoeen99@gmail.com>
2025-01-13 16:19:43 +0200

    edit

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -6,6 +6,7 @@ use App\Models\Brand;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 
 class ShopController extends Controller
 {
@@ -15,6 +16,9 @@ class ShopController extends Controller
         $o_order="";
         $order= $request->query('order') ? $request->query('order') : -1;
         $f_brands = $request->query('brands');
+        $f_categories= $request->query('categories');
+        $min_price = $request->query('min')? $request->query('min'):1;
+        $max_price = $request->query('max')? $request->query('max'):500;
         switch($order){
             case 1:
                 $o_column='created_at';
@@ -37,11 +41,19 @@ class ShopController extends Controller
                 $o_order='DESC';
         };
         $brands= Brand::orderBy('name','ASC')->get();
+        $categories = Category::orderBy('name','ASC')->get();
         $products=Product::where(function ($query) use($f_brands) {
             $query->whereIn('brand_id',explode(',',$f_brands))->orWhereRaw("'".$f_brands."'=''");
         })
+        ->where(function ($query) use($f_categories) {
+            $query->whereIn('category_id',explode(',',$f_categories))->orWhereRaw("'".$f_categories."'=''");
+        })
+        ->Where(function($query)use($min_price,$max_price){
+            $query->whereBetween('regular_price',[$min_price,$max_price])
+            ->orWhereBetween('sale_price',[$max_price,$max_price]);
+        })
         ->orderBy($o_column,$o_order)->paginate($size);
-        return view('admin.shop.index',compact('products','size','order','brands','f_brands'));
+        return view('admin.shop.index',compact('products','size','order','brands','f_brands','categories','f_categories','min_price','max_price'));
     }
     public function product_details($product_slug){
         $product= Product::where('slug',$product_slug)->first();
--- resources/views/admin/shop/index.blade.php
+++ resources/views/admin/shop/index.blade.php
@@ -27,38 +27,18 @@
                         </h5>
                         <div id="accordion-filter-1" class="accordion-collapse collapse show border-0"
                             aria-labelledby="accordion-heading-1" data-bs-parent="#categories-list">
-                            <div class="accordion-body px-0 pb-0 pt-3">
+                            <div class="accordion-body px-0 pb-0 pt-3 category-list">
                                 <ul class="list list-inline mb-0">
+                                    @foreach ($categories as $category)
                                     <li class="list-item">
-                                        <a href="#" class="menu-link py-1">Dresses</a>
+                                        <span class="menu-link py-1">
+                                            <input type="checkbox" class="chk-category[]" name="categories" value="{{$category->id}}"
+                                            @if (in_array($category->id,explode(',',$f_categories))) checked="checked" @endif />
+                                            {{$category->name}}
+                                        </span>
+                                        <span class="text-right float-end">{{$category->products->count()}}</span>
                                     </li>
-                                    <li class="list-item">
-                                        <a href="#" class="menu-link py-1">Shorts</a>
-                                    </li>
-                                    <li class="list-item">
-                                        <a href="#" class="menu-link py-1">Sweatshirts</a>
-                                    </li>
-                                    <li class="list-item">
-                                        <a href="#" class="menu-link py-1">Swimwear</a>
-                                    </li>
-                                    <li class="list-item">
-                                        <a href="#" class="menu-link py-1">Jackets</a>
-                                    </li>
-                                    <li class="list-item">
-                                        <a href="#" class="menu-link py-1">T-Shirts & Tops</a>
-                                    </li>
-                                    <li class="list-item">
-                                        <a href="#" class="menu-link py-1">Jeans</a>
-                                    </li>
-                                    <li class="list-item">
-                                        <a href="#" class="menu-link py-1">Trousers</a>
-                                    </li>
-                                    <li class="list-item">
-                                        <a href="#" class="menu-link py-1">Men</a>
-                                    </li>
-                                    <li class="list-item">
-                                        <a href="#" class="menu-link py-1">Jumpers & Cardigans</a>
-                                    </li>
+                                    @endforeach
                                 </ul>
                             </div>
                         </div>
@@ -202,16 +182,16 @@
                         <div id="accordion-filter-price" class="accordion-collapse collapse show border-0"
                             aria-labelledby="accordion-heading-price" data-bs-parent="#price-filters">
                             <input class="price-range-slider" type="text" name="price_range" value=""
-                                data-slider-min="10" data-slider-max="1000" data-slider-step="5"
-                                data-slider-value="[250,450]" data-currency="$" />
+                                data-slider-min="1" data-slider-max="500" data-slider-step="5"
+                                data-slider-value="[{{$min_price}},{{$max_price}}]" data-currency="$" />
                             <div class="price-range__info d-flex align-items-center mt-2">
                                 <div class="me-auto">
                                     <span class="text-secondary">Min Price: </span>
-                                    <span class="price-range__min">$250</span>
+                                    <span class="price-range__min">$1</span>
                                 </div>
                                 <div>
                                     <span class="text-secondary">Max Price: </span>
-                                    <span class="price-range__max">$450</span>
+                                    <span class="price-range__max">$500</span>
                                 </div>
                             </div>
                         </div>
@@ -490,6 +470,9 @@
         <input type="hidden" name="size" id="size" value="{{ $size }}" />
         <input type="hidden" name="order" id="order" value="{{$order}}"/>
         <input type="hidden" name="brands" id="hdnBrands"/>
+        <input type="hidden" name="categories" id="hdnCategories"/>
+        <input type="hidden" name="min" id="hdnMinPrice" value="{{$min_price}}"/>
+        <input type="hidden" name="max" id="hdnMaxPrice" value="{{$max_price}}"/>
     </form>
 @endsection
 @push('scripts')
@@ -516,6 +499,30 @@
                 $("#hdnBrands").val(brands);
                 $("#frmfilter").submit();
             });
+
+            $("input[name='categories']").on("change",function(){
+                var categories ="";
+                $("input[name='categories']:checked").each(function(){
+                    if(categories==""){
+                        categories += $(this).val();
+                    }
+                    else{
+                        categories += "," + $(this).val();
+                    }
+                });
+                $("#hdnCategories").val(categories);
+                $("#frmfilter").submit();
+            });
+
+            $("[name='price_range']").on("change",function(){
+                var min= $(this).val().split(',')[0];
+                var max=$(this).val().split(',')[1];
+                $("#hdnMinPrice").val(min);
+                $("#hdnMaxPrice").val(max);
+                setTimeout(() => {
+                    $("#frmfilter").submit();
+                }, 2000);
+            })
         });
     </script>
 @endpush
